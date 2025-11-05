6dda7bec486564afded2aafddf6cc1c1a257afd2
khnningr <barrios12khnningr@proton.me>
2025-11-05 02:25:35 -0600

    2025/11/05 a las 02:25 a.m.

--- .local/bin/desktop/03-desktop-setup/fonts.sh
+++ .local/bin/desktop/03-desktop-setup/fonts.sh
@@ -3,22 +3,28 @@
 set -euo pipefail
 
 pkg_fonts=(
-    "gnu-free-fonts"
+	"gnu-free-fonts"
 	"noto-fonts"
-	"noto-fonts-cjk"
-	"noto-fonts-emoji"
+	"noto-fonts-cjk"   # soporte a carácteres asiaticos.
+	"noto-fonts-emoji" # Soporte a emojis.
 	"noto-fonts-extra"
 	"nerd-fonts"
 	"ttf-nerd-fonts-symbols"
 )
 
-if command -v "pacman" &> /dev/null; then
-    sudo pacman -S --needed "${pkg_fonts[@]}"
+if command -v "pacman" &>/dev/null; then
+	sudo pacman -S --needed "${pkg_fonts[@]}"
 
-elif command -v "dnf" &> /dev/null; then
-    sudo dnf install -y
+elif command -v "dnf" &>/dev/null; then
+	sudo dnf install -y
 
 else
-    echo "Distro no soportada."
-    exit 1
+	echo "Distro no soportada."
+	exit 1
+fi
+
+FONT_PATH=$(fc-list | grep -i "NotoSansCJK-Regular.ttc" | cut -d: -f1 | head -n1)
+if [[ -n "$FONT_PATH" ]] && command -v steam; then
+	mkdir -p ~/.steam/steam/steamui/fonts
+	cp "$FONT_PATH" ~/.steam/steam/steamui/fonts/
 fi
